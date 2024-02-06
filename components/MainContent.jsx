1e43986e0243d0031a1fd079597fe9d5a53f05e9
IVAN <danilchenco36@gmai.com>
2024-02-06 14:19:09 +0600

    Изменения

---
+++ components/MainContent.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Main (){
+    return (
+        <div>
+            <input type="checkbox" />
+            <p>txt</p>
+            <input type="checkbox" />
+            <p>txt</p>
+            <input type="checkbox" />
+            <p>txt</p>
+            <input type="checkbox" />
+            <p>txt</p>
+        </div>
+    )
+}
+ export default Main
